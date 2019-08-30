a36f3e9b5a031bf66e9c52c7e61443493cca3166
luHan <luhan@caiyikeji.com>
2019-08-30 10:21:24 +0800

    添加跨域处理

---
+++ src/main/java/com/luhan/filter/CorsHanlderFilter.java
@@ -0,0 +1,40 @@
+package com.luhan.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 〈跨域过滤器处理〉<br>
+ *
+ * @author luHan
+ * @create 2019-08-30 09:46
+ * @since 1.0.0
+ */
+@Configuration
+public class CorsHanlderFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
